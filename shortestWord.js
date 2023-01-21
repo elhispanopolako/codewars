200b445c9fe3582e2f2658d4f69b861f092d34e4
twozniak <twozniak@consdata.com>
2023-01-21 13:46:25 +0100

    shortest word

---
+++ shortestWord.js
@@ -0,0 +1,18 @@
+function findShort(s) {
+    const words = s.split(' ')
+    const wordsLength = words.map((word) => word.length)
+    return Math.min(...wordsLength)
+}
+
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe("Basic tests", () => {
+    it("Testing for fixed tests", () => {
+        assert.strictEqual(findShort("bitcoin take over the world maybe who knows perhaps"), 3);
+        assert.strictEqual(findShort("turns out random test cases are easier than writing out basic ones"), 3);
+        assert.strictEqual(findShort("Let's travel abroad shall we"), 2);
+    })
+})
